25fb6deff909273c2705a16e3197fbb193c25161
gfxv <superfreezu@gmail.com>
2023-06-27 20:18:59 +0300

    presentations

--- JDBC-Test/src/main/java/App.java
+++ JDBC-Test/src/main/java/App.java
@@ -1,19 +1,10 @@
 import java.sql.*;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Stream;
 
 
 public class App {
 
-
     public static void main(String[] args) throws SQLException {
-        Stream.of("kilo", "mega", "giga", "tera", "peta", "exa", "zetta", "yotta")
-                .filter(s -> s.length() == 4)
-                .map(s -> "11" + s)
-                .skip(2)
-                .sorted()
-                .forEachOrdered(System.out::print);
+
     }
 
 }
